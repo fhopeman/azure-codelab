30b257e641adfddcda12834d3ae147f522e88535
Fabian Hoffmann <fabian.hoffmann@otto.de>
2019-04-02 15:06:05 +0200

    add 8080 in security group

--- virtual-machine/network.tf
+++ virtual-machine/network.tf
@@ -38,6 +38,18 @@ resource "azurerm_network_security_group" "yocto" {
     source_address_prefix      = "${var.myIp}"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "${var.myIp}"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "yocto" {
